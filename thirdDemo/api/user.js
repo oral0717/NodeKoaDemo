445445981cd9b9f323d8e3dca141e8b210c815c2
oral0717 <807910824@qq.com>
2020-02-01 22:19:52 +0800

    thirdDemo完善注册接口,及中间层

--- thirdDemo/api/user.js
+++ thirdDemo/api/user.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router')
 const md5 = require('md5-node')
-
+const axios = require('axios')
+const querystring = require('querystring')
 const router = new Router()
 
 router.get('/index', ctx => {
@@ -16,7 +17,8 @@ router.post('/register',async ctx=>{
   password = md5(password)
 
   let data = await ctx.db.query(`SELECT user FROM admin WHERE user = '${username}' AND password = '${password}' LIMIT 1`)
-  if(data.length > 0){
+
+  if(data.length > 0 && data.user ){
     return ctx.body = {
       code: 100,
       message: '用户名已经存在'
@@ -34,6 +36,47 @@ router.post('/register',async ctx=>{
   }
 })
 
-// 中间层
+// 中间层,包装下返回的接口，使得接口可读性更强，对前端更友好
+router.post('/reg', async ctx =>{
+  let {username,password} = ctx.request.body
+  console.log(username,password)
+  if(!username || !password){
+    return ctx.body = {
+      code:3,
+      message:'请输入用户名和密码'
+    }
+  }
+
+  let {data} = await axios({
+    url: "http://106.54.210.212/api/v1/user/register.php",
+    method:"post",
+    data:{
+      username,
+      password
+    },
+    transformRequest:[
+      data =>{
+        return querystring.stringify(data)
+      }
+    ]
+  })
+
+  if (data.code === 0) {
+    return ctx.body = {
+      code: 0,
+      message: '用户名已经存在'
+    }
+  }
+
+  if (data.code === 1) {
+    return ctx.body = {
+      code: 1,
+      message: '注册成功'
+    }
+  }
+
+})
+
+
 
 module.exports = router.routes()
